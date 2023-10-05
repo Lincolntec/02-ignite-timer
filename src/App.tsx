de41bf642491e89c792b56b2cd6f1caceb8b6bb8
Alcino José Lincoln de Souza <lincoln_tec@hotmail.com>
2023-10-05 19:58:08 -0300

    Contexto  no React

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
+import { CyclesContext } from './pages/Home'
+import { createContext, useState } from 'react'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
